1b77d386ab77d0e0bb69eaa3f407a3aa34095c2b
Rafael Cerqueira <rafaelcerqueiraf@outlook.com>
2025-01-14 20:27:22 -0300

    Refatorando a classe Principal

--- src/main/java/br/com/alura/screenmatch/principal/Principal.java
+++ src/main/java/br/com/alura/screenmatch/principal/Principal.java
@@ -1,14 +1,17 @@
 package br.com.alura.screenmatch.principal;
 
 import br.com.alura.screenmatch.Config;
+import br.com.alura.screenmatch.model.DadosEpisodio;
 import br.com.alura.screenmatch.model.DadosSerie;
 import br.com.alura.screenmatch.model.DadosTemporada;
 import br.com.alura.screenmatch.service.ConsumoApi;
 import br.com.alura.screenmatch.service.ConverteDados;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Principal {
 
@@ -18,14 +21,12 @@ public class Principal {
     private Scanner leitura = new Scanner(System.in);
     private ConverteDados conversor = new ConverteDados();
 
-
     public void exibeMenu() {
         System.out.println("Digite o nome da Série para busca: ");
         var nomeSerie = leitura.nextLine();
         var json = consumo.obterDados(ENDERECO + nomeSerie.replace(" ", "+") + API_KEY);
         DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
         System.out.println(dados);
-
 
 		List<DadosTemporada> temporadas = new ArrayList<>();
 
@@ -35,5 +36,25 @@ public class Principal {
 			temporadas.add(dadosTemporada);
 		}
 		temporadas.forEach(System.out::println);
+
+//        for (int i = 0; i < dados.totalTemporadas(); i++) {
+//            List<DadosEpisodio> episodiosTemorada = temporadas.get(i).episodios();
+//            for (int j = 0; j < episodiosTemorada.size(); j++) {
+//                System.out.println(episodiosTemorada.get(j).titulo());
+//            }
+//        }
+
+        temporadas.forEach(t -> t.episodios().forEach(e -> System.out.println(e.titulo())));
+
+        List<DadosEpisodio> dadosEpisodios = temporadas.stream()
+                .flatMap(t -> t.episodios().stream())
+                .collect(Collectors.toList());
+
+        System.out.println("\nTop 5 episódios:");
+        dadosEpisodios.stream()
+                .filter(e -> !e.avaliacao().equalsIgnoreCase("N/A"))
+                .sorted(Comparator.comparing(DadosEpisodio::avaliacao).reversed())
+                .limit(5)
+                .forEach(System.out::println);
     }
 }
